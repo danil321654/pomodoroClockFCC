e1902bfa4839a516d3f67a3f54c67421bef374e3
danil321654 <danil321654@gmail.com>
2020-02-01 01:59:49 +0300

    crated alpha interface; start button works for now

--- src/PomodoroClock.js
+++ src/PomodoroClock.js
@@ -1,44 +1,23 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
 
-const render = (min,sec) => {
-  if(sec.toString().length == 1) sec = '0'+sec;
-ReactDOM.render(
-<div>
-  <PomodoroClock sec={sec} min={min}/>,
-</div>,
-document.getElementById("root")
-);
-
-setTimeout(timer,1000,min,sec);}
-
-function timer  (min, sec)  {
-
-  if(sec==0){ sec = 60;min--;}
-  sec--;
-  console.log(min,sec);
-  render(min,sec);
-}
 class PomodoroClock extends React.Component {
   constructor(props){
     super(props);
-    this.state = {
-      secLength: 25,
-      curBreakLength: 5,
-      breakLength: 500,
-      last: "RESET"
-    };
     this.handleClick = this.handleClick.bind(this);
   }
   handleClick(event){
-    console.log('sad');
-    timer(
-      this.props.min,
-      this.props.sec);
+    this.props.updateScr({type: "STARTSTOP"});
   }
-  componentWillUpdate(){
-    console.log("update");
+  componentDidMount(){
+
+  }
+  componentDidUpdate(){
+    setTimeout( () => {
+      this.props.curSesLengthSec--;
+      if (this.props.curSesLengthSec == 59)
+        this.props.curSesLengthMin--;
+
+    },1000)
   }
   render() {
     return (
@@ -72,8 +51,7 @@ class PomodoroClock extends React.Component {
         </div>
         <div className="session">
           <div>Session</div>
-          <div className="timer">{
-            this.props.min}:{this.props.sec}</div>
+          <div className="timer">{this.props.curSesLengthMin}:{this.props.curSesLengthSec}</div>
           <div >
             <button className="button" onClick={this.handleClick} value="STARTSTOP"><div >
               <i class="fa fa-play fa-play" aria-hidden="true" ></i>
@@ -89,9 +67,4 @@ class PomodoroClock extends React.Component {
   }
 }
 
-ReactDOM.render(
-<div>
-    <PomodoroClock sec="00" min="25"/>,
-</div>,
-  document.getElementById("root")
-);
+export default PomodoroClock;
